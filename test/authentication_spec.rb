17ca33bd795689c7c6f77dd3da6fa21b35d44595
Joey Chen <jay.hatelove@gmail.com>
2020-12-29 23:04:12 +0800

    otp: use RSpec double instead of manual fake object

--- test/authentication_spec.rb
+++ test/authentication_spec.rb
@@ -12,8 +12,12 @@ describe 'Authentication' do
   describe ":is_valid" do
     context "when valid" do
       it 'should be return true' do
-        profile = FakeProfile.new
-        token = FakeToken.new
+        profile = double
+        allow(profile).to receive(:password).with('joey').and_return('91')
+
+        token = double
+        allow(token).to receive(:random_token).and_return('0' * 6)
+
         authentication = AuthenticationService.new(profile, token)
         is_valid = authentication.valid?('joey', '91000000')
         expect(is_valid).to be(true)
@@ -23,17 +27,3 @@ describe 'Authentication' do
 
 end
 
-class FakeProfile < ProfileDao
-  def password(account)
-    if account == 'joey'
-      return '91'
-    end
-    raise NotImplementedError
-  end
-end
-
-class FakeToken < RsaTokenDao
-  def random_token(account)
-    '0' * 6
-  end
-end
