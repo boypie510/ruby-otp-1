5dee6b6f3417801bd996b5a56c851187fe99e388
Joey Chen <jay.hatelove@gmail.com>
2020-12-29 22:34:02 +0800

    holiday: refactor test

--- test/holiday_spec.rb
+++ test/holiday_spec.rb
@@ -2,9 +2,17 @@ require 'rspec'
 require '../lib/holiday'
 require 'date'
 
+def given_today(month, day)
+  allow(Date).to receive(:today).and_return(Date.new(2000, month, day))
+end
+
+def say_hello_should_be(expected)
+  expect(@holiday.say_hello).to eq(expected)
+end
+
 describe 'Holiday' do
   before do
-    # Do nothing
+    @holiday = Holiday.new
   end
 
   after do
@@ -14,9 +22,8 @@ describe 'Holiday' do
   describe ':say_hello' do
     context 'when today is Xmas' do
       it 'should return Merry Xmas' do
-        holiday = Holiday.new
-        allow(Date).to receive(:today).and_return(Date.new(2000, 12, 25))
-        expect(holiday.say_hello).to eq('Merry Xmas')
+        given_today(12, 25)
+        say_hello_should_be('Merry Xmas')
       end
     end
 
